304ec9cd26c5168aaf6cd4b9b210ab78bbad53de
Jhonatan Mota <BR1050009@totvspartners.com.br>
2022-03-09 08:21:26 -0300

    starting project

---
+++ src/test/java/com/test/corp/voting/VotingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.corp.voting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
